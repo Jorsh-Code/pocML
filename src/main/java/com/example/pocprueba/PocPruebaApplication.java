8e091cd1d07ef3d83ef8d98580bc66cfcfdcd23f
Jorge Luis Hernandez Martinez <lhernandez@na-at.com.mx>
2023-02-10 18:24:48 -0600

    primero

---
+++ src/main/java/com/example/pocprueba/PocPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocPruebaApplication.class, args);
+    }
+
+}
